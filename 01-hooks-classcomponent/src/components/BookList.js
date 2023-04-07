eb080ad04b4c5ca6ce44e554e9cf902d256a443b
Charan Shetty <charan.shetty@fluke.com>
2023-04-07 23:19:38 +0530

    changes : code clean

---
+++ 01-hooks-classcomponent/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+class BookList extends Component {
+   render(){
+    return(
+        <div className="book-list">
+            <ul>
+                <li>Poorna chandrA THEjaswi </li>
+                <li>Yana the great journey</li>
+                <li>Wings of fire</li>
+            </ul>
+            </div>
+    );
+   }
+}
+ 
+export default BookList;
