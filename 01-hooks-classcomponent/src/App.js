1abce0d851487f10faf989ceb30520b6ac7edbec
charanshetty91 <charan28shetty@gmail.com>
2023-04-08 13:23:31 +0530

    multi context provider and consumer technique

--- 01-hooks-classcomponent/src/App.js
+++ 01-hooks-classcomponent/src/App.js
@@ -2,15 +2,20 @@ import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import ThemeContextProvider from './contexts/Theamcontexts';
 import ToggleTheme from './components/ThemeToggle';
+import AuthContextProvider from './contexts/Authcontexts';
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>      
-        <Navbar />
-        <BookList /> 
-        <ToggleTheme />
+      {/* consumed two different provider to get global data */}
+      <ThemeContextProvider> 
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ToggleTheme />
+        </AuthContextProvider>
       </ThemeContextProvider>
+    
     </div>
   );
 }
--- 01-hooks-classcomponent/src/components/Navbar.js
+++ 01-hooks-classcomponent/src/components/Navbar.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/Theamcontexts';
+import {AuthContext} from '../contexts/Authcontexts';
 
 class Navbar extends Component {
-    static contextType = ThemeContext;
+    //static contextType = ThemeContext;
     render() {
-        return(<ThemeContext.Consumer>{(context) => {
-            const { isLightTheme, light, dark } = context;
-            const theme = isLightTheme ? light : dark;
-            return (
-                <nav style={{ background: theme.ui, color: theme.syntax }}>
-                    <h1>Context App</h1>
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                    </ul>
-                </nav>
-            );
-        }}
-        </ThemeContext.Consumer>);       
+
+        return(
+        <AuthContext.Consumer>{(authContext)=>(
+            <ThemeContext.Consumer>{(themeContext) => {
+                const {isAuthenticated,DoAuth} = authContext;
+                const { isLightTheme, light, dark } = themeContext;
+                const theme = isLightTheme ? light : dark;
+                return (
+                    <nav style={{ background: theme.ui, color: theme.syntax }}>
+                        <h1>Context App</h1>
+                        <div onClick = {DoAuth}>{isAuthenticated ? 'LoggedIn' : 'LoogedOut'}</div>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                );
+            }}
+            </ThemeContext.Consumer>            
+        )}</AuthContext.Consumer>
+       );       
 
     }
 }
