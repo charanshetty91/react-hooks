681426dabaece03cfd1df1381fd16bfc513b5b8d
charanshetty91 <charan28shetty@gmail.com>
2023-04-08 14:33:09 +0530

    hooks app changes

---
+++ 02-hooks-app/src/components/song-list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SongsList = () =>{
+    return (
+        <div>
+            <ul>
+                <li>Yarivanu yarivanu</li>
+                <li>yarige yaroontu</li>
+            </ul>
+        </div>
+    );
+}
+export default SongsList;
