4ab645630baf4c8c6335a87369f5e39b2c351b07
charanshetty91 <charan28shetty@gmail.com>
2023-04-09 07:56:01 +0530

    useeffect hooks changes

--- 02-hooks-app/src/components/song-list.js
+++ 02-hooks-app/src/components/song-list.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // import { v4 as uuidv4 } from 'uuid';
 import AddSongsForm from './new-songs-form';
 
@@ -11,10 +11,16 @@ const SongsList = () => {
         { name: "yarige yaroontu", id: 2 },
         { name: "adisitata nesara", id: 3 }
     ]);
+    const [count, AddCount] = useState(21);
     const AddSongs = (song) => {
-        console.log(song);
         UpdateSongList([...songList, song])
     }
+
+    useEffect(() => { console.log('useeffect run for count', +count);},[count ])
+
+    useEffect(() => {
+        console.log('use effect hook ran', ...songList);
+    }, [songList])
     return (
         <div>
             <ul>
@@ -22,7 +28,8 @@ const SongsList = () => {
                     return (<li key={song.id}>{song.name}</li>);
                 })}
             </ul>
-            <AddSongsForm AddSongs ={AddSongs}/>
+            <AddSongsForm AddSongs={AddSongs} />
+            <button onClick={() => { AddCount(count + 1) }}>count increase to 1 :{count} </button>
         </div>
     );
 }
