35923461f1f3056963abe7c6f4d62c1bb91072de
charanshetty91 <charan28shetty@gmail.com>
2023-04-09 10:03:22 +0530

    13- useContext example

--- 01-hooks-classcomponent/src/components/BookList.js
+++ 01-hooks-classcomponent/src/components/BookList.js
@@ -1,23 +1,48 @@
-import React, { Component } from "react";
+import React, { Component ,useContext} from "react";
 import { ThemeContext } from "../contexts/Theamcontexts";
-class BookList extends Component {
 
-    static contextType = ThemeContext;
+//useContext hooks use , this will remove static contexttype object creation 
+const BookList = () => {
+    const {isLightTheme,dark,light} = useContext(ThemeContext);
+    const theme = isLightTheme ?light : dark;
+    return (
 
-    render() {
-        const { isLightTheme, dark, light } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-
-            <div className="book-list" style={{color:theme.syntax,  background : theme.bg}}>
-                <ul>
-                    <li style={{background:theme.ui}}>Poorna chandrA THEjaswi </li>
-                    <li style={{background:theme.ui}}>Yana the great journey</li>
-                    <li style={{background:theme.ui}}>Wings of fire</li>
-                </ul>
-            </div>
-        );
-    }
+        <div className="book-list" style={{color:theme.syntax,  background : theme.bg}}>
+            <ul>
+                <li style={{background:theme.ui}}>Poorna chandrA THEjaswi </li>
+                <li style={{background:theme.ui}}>Yana the great journey</li>
+                <li style={{background:theme.ui}}>Wings of fire</li>
+            </ul>
+        </div>
+    );
 }
-
+ 
 export default BookList;
+
+
+
+
+// import React, { Component} from "react";
+// import { ThemeContext } from "../contexts/Theamcontexts";
+
+// class BookList extends Component {
+
+//     static contextType = ThemeContext;
+
+//     render() {
+//         const { isLightTheme, dark, light } = this.context;
+//         const theme = isLightTheme ? light : dark;
+//         return (
+
+//             <div className="book-list" style={{color:theme.syntax,  background : theme.bg}}>
+//                 <ul>
+//                     <li style={{background:theme.ui}}>Poorna chandrA THEjaswi </li>
+//                     <li style={{background:theme.ui}}>Yana the great journey</li>
+//                     <li style={{background:theme.ui}}>Wings of fire</li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
+
+// export default BookList;
