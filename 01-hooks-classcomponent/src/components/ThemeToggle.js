17aee7184e34746a3cf13c97cc89ac33f1a1fb55
charanshetty91 <charan28shetty@gmail.com>
2023-04-09 10:24:06 +0530

    14-useContext use on toggleTheme class

--- 01-hooks-classcomponent/src/components/ThemeToggle.js
+++ 01-hooks-classcomponent/src/components/ThemeToggle.js
@@ -1,16 +1,32 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/Theamcontexts";
 
-class ToggleTheme extends Component {
-    //inbuilt property 'contextType' should use as same
-    static contextType = ThemeContext;
-    render() {
-        const {toggleThemefunc} = this.context;
+const ToggleTheme = () => {    
+    const {toggleThemefunc} = useContext(ThemeContext);
+    
         return (
 
             <button onClick={toggleThemefunc}>Toggle the Theme</button>
         );
-    }
+    
 }
 
 export default ToggleTheme;     
+
+
+// import React, { Component,useContext } from "react";
+// import { ThemeContext } from "../contexts/Theamcontexts";
+
+// class ToggleTheme extends Component {
+//     //inbuilt property 'contextType' should use as same
+//     static contextType = ThemeContext;
+//     render() {
+//         const {toggleThemefunc} = this.context;
+//         return (
+
+//             <button onClick={toggleThemefunc}>Toggle the Theme</button>
+//         );
+//     }
+// }
+
+// export default ToggleTheme;     
